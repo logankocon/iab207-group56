02d7796213c2ffa8bf334194d0d5d284bfd4db04
Joshua <walsh.joshua.aaron@gmail.com>
2023-10-30 21:11:51 +1100

    created database file in new instance/ directory and created new models in models.py

--- website/models.py
+++ website/models.py
@@ -1,2 +1,42 @@
-class User():
+from . import db
+from datetime import datetime
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
     __tablename__ = 'users' # good practice to specify table name
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), index=True, unique=True, nullable=False)
+    emailid = db.Column(db.String(100), index=True, nullable=False)
+
+
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    comments = db.relationship('Comment', backref='user')
+
+
+class Event(db.Model):
+    __tablename__ = 'events'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    description = db.Column(db.String(200))
+    image = db.Column(db.String(400))
+
+    event_date = db.Column(db.DateTime, default=datetime.now())
+
+    max_tickets = db.Column(db.Integer)
+    tickets_left = db.Column(db.Integer)
+
+    creator_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key = True)
+    text = db.Column(db.String(400))
+    created_at = db.Column(db.DateTime, default=datetime.now())
+
+    #fk's
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
