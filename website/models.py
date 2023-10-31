8997fac83bb85ed6cc952f98722a96e4c7a35198
logankocon <logankocon@gmail.com>
2023-10-31 10:36:59 +1000

    added login functionality

--- website/models.py
+++ website/models.py
@@ -8,10 +8,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), index=True, unique=True, nullable=False)
     emailid = db.Column(db.String(100), index=True, nullable=False)
-
-
     password_hash = db.Column(db.String(255), nullable=False)
-
     comments = db.relationship('Comment', backref='user')
 
 
