dcb75a8afd1161e9ac05500e45edd8effe69e22f
Joshua <walsh.joshua.aaron@gmail.com>
2023-11-01 15:36:42 +1100

    front page now shows events in the database, updated views.py to reflect this too

--- website/views.py
+++ website/views.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
+from .models import Event
+from . import db
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
-    return render_template('index.html')
+    events = db.session.scalars(db.select(Event)).all()
+    return render_template('index.html', events = events)
