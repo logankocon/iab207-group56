b53374432908310aa25e6c9086efecffd5fe4767
JoshDaSneaky <walsh.joshua.aaron@gmail.com>
2023-11-02 22:29:01 +1000

    transfering to a new branch to move files to another computer without commiting to the main branch, ignore me

--- website/views.py
+++ website/views.py
@@ -8,3 +8,21 @@ main_bp = Blueprint('main', __name__)
 def index():
     events = db.session.scalars(db.select(Event)).all()
     return render_template('index.html', events = events)
+
+@main_bp.route('/search')
+def search():
+    search = request.args.get('search', default='', type=str)
+    filter_option = request.args.get('filter', default='description', type=str)
+
+    if search == '':
+        if filter_option == 'Filter by Genre':
+            events = db.session.scalars(db.select(Event)).all()
+        else:
+            query = '%' + filter_option + '%'
+            events = Event.query.filter(Event.genre.like(query)).all()
+
+        return render_template('index.html', events = events)
+    print(search)
+    print(filter_option)
+
+    return redirect(url_for('main.index'))
