923913d5f1453e59201d6c3618a5600d1bb4dde7
Joshua <walsh.joshua.aaron@gmail.com>
2023-11-03 01:26:20 +1100

    search and filter work, EVEN WHEN USED TOGETHERgit add website/templates/index.html git add website/templates/index.html  IM SO SMART :D

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
 from .models import Event
 from . import db
+from sqlalchemy import or_, and_
 
 main_bp = Blueprint('main', __name__)
 
@@ -9,3 +10,48 @@ def index():
     events = db.session.scalars(db.select(Event)).all()
     return render_template('index.html', events = events)
 
+
+@main_bp.route('/search')
+def search():
+    search = request.args['search']
+    filter_option = request.args.get('filter', default='description', type=str)
+
+    if search == '':
+        
+        if filter_option == 'Filter by Genre':
+            events = db.session.scalars(db.select(Event)).all()
+        else:
+            query = '%' + filter_option + '%'
+            events = Event.query.filter(Event.genre.like(query)).all()
+         
+        return render_template('index.html', events = events)
+    else:
+        if filter_option == 'Filter by Genre':
+            query = '%' + search + '%'
+            
+            #select all where (event name OR description match)
+            events = Event.query.filter(
+                        or_(Event.name.like(query), Event.description.like(query))
+                    ).all()
+
+        else:
+            #select all where (event name OR description match) AND (genre matches)
+            search_query = '%' + search + '%'
+            filter_query = '%' + filter_option + '%'
+            
+           
+            events = Event.query.filter(
+                        and_(
+                            or_(
+                                Event.name.like(search_query),
+                                Event.description.like(search_query)
+                            ),
+                            Event.genre.like(filter_query)
+                        )
+                    ).all()
+
+        return render_template('index.html', events = events)
+
+
+
+    return redirect(url_for('main.index'))
